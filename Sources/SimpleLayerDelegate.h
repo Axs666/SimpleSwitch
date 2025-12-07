49f9bb336269f8fbc42f4ace7e90e9cab5f478f8
A先生 <272685620@qq.com>
2025-12-07 16:18:28 +0800

    Update SimpleLayerDelegate.h

--- Sources/SimpleLayerDelegate.h
+++ Sources/SimpleLayerDelegate.h
@@ -4,10 +4,6 @@
 @interface SimpleLayerDelegate : NSObject <CALayerDelegate>
 
 @property (nonatomic) BOOL animated;
-@property (readonly) unsigned long long hash;
-@property (readonly) Class superclass;
-@property (readonly, copy) NSString *description;
-@property (readonly, copy) NSString *debugDescription;
 
 - (id)actionForLayer:(id)a0 forKey:(id)a1;
 
